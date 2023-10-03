dbbcbf5c090f97dad78aa22f8c20defa8d3866e6
Виктория <viksyaavito@gmail.com>
2023-10-04 00:02:27 +0300

    Start

---
+++ src/main/java/ru/netology/services/HolidayService.java
@@ -0,0 +1,18 @@
+package ru.netology.services
+public class HolidayService {
+    public int calculate(int income,int expenses,int threshold) {
+        int money = 0;
+        for (int i = 0; i < 12; i++) {
+            if(money >= threshold) {
+
+
+            }
+
+                else {
+
+                    money = money + income;
+            }
+         return i;
+        }
+    }
+}
